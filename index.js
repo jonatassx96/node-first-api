b34688abbbd13f09e3222a857f8574ba54d56c3b
jonatassx96 <jonatas.sx96@gmail.com>
2022-12-05 09:31:39 -0300

    chore: test git

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const port = 3001
 
 const app = express()
 app.get('/users', (request, response) => {
-    return response.send('Hello node!!!')
+    return response.send('Hello node!')
 })
 app.listen(port, () =>{
     console.log(`Server started on port ${port}.`)
 })
 
+//TESTE GIT
