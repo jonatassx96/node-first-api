a283cbfac5b29d736ea1db8233d8a9232bae68ec
Jonatas Xavier <81437599+jonatassx96@users.noreply.github.com>
2022-12-05 09:28:14 -0300

    chore: inicio da api

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const port = 3001
+
+const app = express()
+app.get('/users', (request, response) => {
+    return response.send('Hello node!!!')
+})
+app.listen(port, () =>{
+    console.log(`Server started on port ${port}.`)
+})
+
