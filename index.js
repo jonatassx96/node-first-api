491883f06903cad2f94316f898b2ee04f2fd77f7
jonatassx96 <jonatas.sx96@gmail.com>
2023-01-02 16:50:35 -0300

    chore

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express = require('express')
-const port = 3001
+const express = require("express");
+const uuid = require("uuid");
 
-const app = express()
-app.get('/users', (request, response) => {
-    return response.send('Hello node!')
-})
-app.listen(port, () =>{
-    console.log(`Server started on port ${port}.`)
-})
+const port = 3001;
+const app = express();
+app.use(express.json());
 
-//TESTE GIT
+const users = [];
+
+app.get("/users", (request, response) => {
+  return response.json(users);
+});
+
+app.post("/users", (request, response) => {
+  const { name, age } = request.body;
+  const user = { id: uuid.v4(), name, age };
+  users.push(user);
+  return response.status(201).json(user);
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}.`);
+});
