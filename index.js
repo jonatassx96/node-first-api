fd6ffe9f81f4734d4f17f9d2ea4f3676f19fd963
jonatassx96 <jonatas.sx96@gmail.com>
2023-01-02 18:57:27 -0300

    add, delete, put

--- index.js
+++ index.js
@@ -18,6 +18,28 @@ app.post("/users", (request, response) => {
   return response.status(201).json(user);
 });
 
+app.put("/users/:id", (request, response) => {
+  const { id } = request.params;
+  const { name, age } = request.body;
+  const updateUser = { id, name, age };
+  const index = users.findIndex((user) => user.id === id);
+  if (index < 0) {
+    return response.status(404).json({ message: "User not found" });
+  }
+  users[index] = updateUser;
+  return response.json(updateUser);
+});
+
+app.delete("/users/:id", (request, response) => {
+  const { id } = request.params;
+  const index = users.findIndex((user) => user.id === id);
+  if (index < 0) {
+    return response.status(404).json({ message: "User not found" });
+  }
+  users.splice(index, 1);
+  return response.status(204).json();
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}.`);
 });
